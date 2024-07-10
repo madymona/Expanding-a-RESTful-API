42c3e81e2089b7ace02c2c24c0a44d671402b361
madymona <madymona@gmail.com>
2024-07-10 15:14:00 -0400

    comments added

--- data/comments.js
+++ data/comments.js
@@ -1,3 +1,64 @@
-const comments = [];
+const comments = [
+    {
+        id: 1,
+        postId: 1,
+        userId: 3,
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet dolor eget neque egestas pretium. Phasellus imperdiet quis justo sit amet tempor."
+    },
+    {
+        id: 2,
+        postId: 2,
+        userId: 1,
+        body: "Praesent a rhoncus sem. In ut tempus ex. Phasellus ultrices, tortor sit amet dapibus interdum, ante dolor gravida leo, fringilla lobortis quam nibh suscipit sapien."
+    },
+    {
+        id: 3,
+        postId: 3,
+        userId: 2,
+        body: "Ut sollicitudin ornare feugiat. Curabitur dignissim risus ut nulla venenatis hendrerit. Proin sed nulla eget sapien condimentum commodo nec in velit. Sed pulvinar lectus et turpis euismod luctus. "
+    },
+    {
+        id: 4,
+        postId: 4,
+        userId: 1,
+        body: "Donec ipsum nisl, mollis vulputate libero nec, fringilla accumsan nisi. Nullam felis mi, suscipit eu euismod a, laoreet id quam. Sed eu erat sollicitudin, mattis nisl a, sodales nisi. Aliquam lacinia tellus gravida nunc consectetur, ut ullamcorper mi varius. Phasellus et arcu efficitur, porta nisi sit amet, blandit mauris. Vivamus a felis orci."
+    },
+    {
+        id: 5,
+        postId: 2,
+        userId: 1,
+        body: "Suspendisse potenti. Suspendisse potenti. Cras feugiat enim risus."
+    },
+    {
+        id: 6,
+        postId: 6,
+        userId: 2,
+        body: "Suspendisse et neque quis eros venenatis placerat vel eget quam. Vivamus eget accumsan neque, et euismod risus. Vivamus eget vestibulum mi. "
+    },
+    {
+        id: 7,
+        postId: 5,
+        userId: 2,
+        body: "In hac habitasse platea dictumst. In posuere pulvinar est, at volutpat est egestas lacinia. Integer ut ipsum vel metus fringilla lobortis. "
+    },
+    {
+        id: 8,
+        postId: 7,
+        userId: 1,
+        body: "Sed elementum elit tellus, vitae cursus enim efficitur in. Pellentesque eget dui a purus venenatis eleifend at vitae quam. Mauris consectetur sapien a varius rutrum."
+    },
+    {
+        id: 9,
+        postId: 8,
+        userId: 2,
+        body: "Morbi dignissim elit pellentesque, accumsan urna eu, facilisis neque. "
+    },
+    {
+        id: 10,
+        postId: 7,
+        userId: 3,
+        body: "Proin eget accumsan orci. Integer et quam sed sem vehicula feugiat. Aenean interdum, ante vitae aliquam venenatis, est justo luctus dui, nec auctor ex odio eu tellus"
+    }
+];
 
 module.exports = comments;
